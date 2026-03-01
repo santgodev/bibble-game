73c38397b37c1e43136262b9a9952a5274b3dd9a
Santiago Guevara <santgodev@example.com>
2026-02-28 22:52:47 -0500

    chore: limpieza de archivos temporales

--- count_words.js
+++
@@ -1,8 +0,0 @@
-
-const { DEFAULT_CATEGORIES } = require('./src/data/categories');
-
-console.log('--- Word Counts per Category ---');
-DEFAULT_CATEGORIES.forEach(cat => {
-    console.log(`${cat.title}: ${cat.words.length} words`);
-});
-console.log('--------------------------------');
